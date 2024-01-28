788200a438c5c2ea5ea710f16395faa797519aad
H-M-Nizum <hmnizum1714032@gmail.com>
2024-01-28 21:31:59 +0600

    user authentication

--- patient/urls.py
+++ patient/urls.py
@@ -7,5 +7,11 @@ router = DefaultRouter()
 router.register('list', views.Patientviewset)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('register/', views.patientRegisterViewsset.as_view(), name='register'), 
+    path('login/', views.UserLoginApiview.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('adminlogin/', views.AdminLoginApiview.as_view(), name='adminlogin'),
+    path('adminlogout/', views.AdminLogoutAPIView.as_view(), name='adminlogout'),
+    path('active/<uid64>/<token>/', views.activate, name='activate')
 ]
